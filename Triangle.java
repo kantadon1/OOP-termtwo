045c93a222e95a83f9ea8515f6e8b372e24c4ab6
kantadon1 <130194109+kantadon1@users.noreply.github.com>
2024-01-21 09:07:05 +0700

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,8 +1,8 @@
 public class Triangle{
     public static void main(String[] args){
-        for(int i=1; i <= 5; i++){
-            for(int j=0; j <= 5+i-1; j++){
-              if(j >= 5 - i+1){
+        for(int i=0; i < 5; i++){
+            for(int j=0; j <= 5+i; j++){
+              if(j >= 5 - i){
                 System.out.print("0");
               }
               else{
